d11674769d9e762cbe35bbeceb3651e223c7f0d5
Martin Nicholson <recyclebing+github@gmail.com>
2017-12-14 01:33:28 +0000

    Unit rotation tool works perfectly

--- js/game.js
+++ js/game.js
@@ -27,12 +27,14 @@ const TEXT = {
 	"fireskull": "Fire Skull. Moves in 2x1 doglegs, jumping over other units to attack its target.",
 	"priest": "Priest. Moves only on diagonals. Continues to where you tell him, regardless of enemies.",
 	"golem": "Golem. Can move in 4 directions (not diagonals), but only 4 steps. Prefers to stay still and use his machine gun.",
-	"queen": "Necromancer. Unlimited movement in 8 directions. Will only kill one enemy per turn.",
-	"king": "Ice Demon. Heavily armoured, he can move or attack 1 square in 8 directions."
+	"necro": "Necromancer. Unlimited movement in 8 directions. Will only kill one enemy per turn.",
+	"iceman": "Ice Man. Heavily armoured, he can move or attack 1 square in 8 directions."
 };
 
 var cells = [];
 var units = [];
+var gameMode = null;
+var guid = 0;
 
 // Build board:
 for (var y=0; y<11; y++) {
@@ -59,30 +61,23 @@ for (var y=0; y<11; y++) {
 
 // Tooltips:
 d.qa("#tools i").forEach(el => {
-	el.addEventListener('mouseover', () => { d.q("#toolstip").innerHTML = TEXT[el.classList[0]]; } );
-	el.addEventListener('mouseout', () => { d.q("#toolstip").innerHTML = ""; } );
-	el.addEventListener('click', () => { setMode('place', el.classList[0]); } );
+	el.addEventListener('mouseover', () => { d.q("#toolstip").innerHTML = TEXT[el.classList[0]]; });
+	el.addEventListener('mouseout', () => { d.q("#toolstip").innerHTML = ""; });
+	el.addEventListener('click', () => { setMode('place', el.classList[0]); });
 });
-
-function piece(className, team=0) {
-	return d.e('i').addClass(className);
-}
-
-function placeAt(x, y, type, team) {
-	d.q("#x"+x+"y"+y).appendChild(piece(type, team));
-}
 
 function setMode(mode, piece="") {
 	while (d.b.c.length > 0) d.b.c.remove(d.b.c[0]);
 	/* Possible modes:
-	place-mine, place-goblin, place-fireskull, place-priest, place-golem, place-queen, place-ice
+	place-mine, place-goblin, place-fireskull, place-priest, place-golem, place-necro, place-iceman
 	rotate
+	move
 	arm
 	*/
 	if (mode == 'place') mode += "-"+piece;
 	d.b.c.add(mode);
 	console.log(mode);
-	//gameMode = mode;
+	gameMode = mode;
 	//messaging.clearMessages();
 }
 
@@ -99,23 +94,26 @@ function highlightPath(x1,y1,x2,y2, piece) {
 }
 
 class Unit {
-	constructor(type, el, x=null, y=null, team=0) {
+	constructor(type, x=null, y=null, team=0) {
 		this.type = type;
-		this.el = el;
+		this.id = type + guid++;
+		this.el = d.e('i').addClass(type);
+		this.el.setAttribute("id", this.id);
+		this.facing = 'ss';
 		this.x = x;
 		this.y = y;
+		if (x !== null && y !== null) this.placeAt(x,y);
 		this.team = team;
 		units.push(this);
 		return this;
 	}
 
 	placeAt(x,y) {
-		d.q("#x"+x+"y"+y).appendChild(this);
+		d.q("#x"+x+"y"+y).appendChild(this.el);
 	}
 
 	_addTo(points) {
-		var me = this;
-		return points.map(p => [me.x+p[0], this.y+p[1]]);
+		return points.map(p => [this.x+p[0], this.y+p[1]]);
 	}
 
 	validMoves() {
@@ -150,6 +148,17 @@ class Unit {
 		// stats battle ATT/DEF
 	}
 
+	rotate() {
+		if (this.type == 'mine') return;
+		var seq = ['ss','ww','nn','ee'];
+		var old = this.facing;
+		var nu = seq[(seq.indexOf(old) + 1) % 4];
+		console.log(nu);
+		swapClass(old, nu, this.el);
+		this.facing = nu;
+		return this;
+	}
+
 	explode() {
 		// Animate blur
 		// Play sound
@@ -157,17 +166,30 @@ class Unit {
 	}
 }
 
-placeAt(0,0,'mine');
-placeAt(1,1,'mine');
-placeAt(3,3,'king');
-placeAt(4,4,'queen');
-placeAt(4,5,'golem');
-placeAt(5,4,'golem');
-placeAt(5,6,'priest');
-placeAt(6,5,'priest');
-placeAt(5,8,'goblin');
-placeAt(6,7,'goblin');
-placeAt(7,6,'goblin');
-placeAt(8,5,'goblin');
-placeAt(7,8,'fireskull');
-placeAt(8,7,'fireskull');
+function swapClass(old, nu, el) {
+	el.classList.remove(old);
+	el.classList.add(nu);
+}
+
+new Unit('mine',0,0);
+new Unit('mine',1,1);
+new Unit('iceman',3,3);
+new Unit('necro',4,4);
+new Unit('golem',4,5);
+new Unit('golem',5,4);
+new Unit('priest',5,6);
+new Unit('priest',6,5);
+new Unit('goblin',5,8);
+new Unit('goblin',6,7);
+new Unit('goblin',7,6);
+new Unit('goblin',8,5);
+new Unit('fireskull',7,8);
+new Unit('fireskull',8,7);
+
+d.qa("#ga i").forEach(el => {
+	el.addEventListener('click', () => {
+		// Find instance:
+		var u = units.filter(u => u.id == el.id)[0];
+		if (gameMode == 'rotate') u.rotate();
+	});
+});
